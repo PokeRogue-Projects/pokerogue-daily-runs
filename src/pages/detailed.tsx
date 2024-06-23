43fb1901aee7051087fb0fb044257bf2b52d7ea4
Jack Dewinter <jackdewinter1@gmail.com>
2024-06-23 17:07:20 +0100

    use follow along text in detailed

--- src/pages/detailed.tsx
+++ src/pages/detailed.tsx
@@ -3,6 +3,17 @@ import { graphql } from "gatsby";
 import Navigation from "../components/Navigation";
 
 const DetailedPage: React.FC<{ data: any }> = ({ data }) => {
+  const stageToWaveMap: { [key: string]: string[] } = {};
+  data.allGoogleSpreadsheetFollowAlong.edges.forEach((edge: any) => {
+    const { wave, waveNumber } = edge.node;
+    if (!wave.startsWith("Wave")) {
+      if (!stageToWaveMap[waveNumber]) {
+        stageToWaveMap[waveNumber] = [];
+      }
+      stageToWaveMap[waveNumber].push(wave);
+    }
+  });
+
   return (
     <div>
       <Navigation />
@@ -34,12 +45,17 @@ const DetailedPage: React.FC<{ data: any }> = ({ data }) => {
             {data.allGoogleSpreadsheetDetailed.edges.map(
               (edge: any, index: any) => {
                 if (edge.node.name) {
+                  const steps =
+                    stageToWaveMap[edge.node.stage]?.map((wave) => (
+                      <React.Fragment key={wave}>
+                        {wave}
+                        <br />
+                      </React.Fragment>
+                    )) || null;
                   return (
                     <tr key={index}>
                       <td style={{ padding: "10px" }}>{edge.node.stage}</td>
-                      <td style={{ padding: "10px" }}>
-                        {edge.node.steps__Guide}
-                      </td>
+                      <td style={{ padding: "10px" }}>{steps}</td>
                       <td style={{ padding: "10px" }}>{edge.node.name}</td>
                       <td style={{ padding: "10px" }}>{edge.node.nature}</td>
                     </tr>
@@ -62,7 +78,14 @@ export const query = graphql`
           name
           stage
           nature
-          steps__Guide
+        }
+      }
+    }
+    allGoogleSpreadsheetFollowAlong {
+      edges {
+        node {
+          wave
+          waveNumber
         }
       }
     }
