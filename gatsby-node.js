90fe14e9b7f866e3b9ce42802b39add18063e52d
Jack Dewinter <jackdewinter1@gmail.com>
2024-06-23 01:08:07 +0100

    add detailed daily guide to graphql data

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,12 +14,18 @@ exports.onPreBootstrap = async ({ reporter }) => {
     const doc = new GoogleSpreadsheet(process.env.DEV_SHEET_ID, serviceAccountAuth);
     await doc.loadInfo();
 
-    const sheet = doc.sheetsByTitle["Follow Along"]
-    await sheet.loadCells("A1")
+    const followAlong = doc.sheetsByTitle["Follow Along"]
+    await followAlong.loadCells("A1")
 
-    const a1 = sheet.getCellByA1('A1');
-    a1.formula = `=IMPORTRANGE("https://docs.google.com/spreadsheets/d/${process.env.DAILY_SHEET_ID}", "'Follow Along'!B:B")`
-    await a1.save()
+    const followAlongA1 = followAlong.getCellByA1('A1');
+    followAlongA1.formula = `=IMPORTRANGE("https://docs.google.com/spreadsheets/d/${process.env.DAILY_SHEET_ID}", "'Follow Along'!B:B")`
+    await followAlongA1.save()
+
+    const detailed = doc.sheetsByTitle["Detailed"]
+    await detailed.loadCells("A1")
+    const detailedA1 = detailed.getCellByA1('A1');
+    detailedA1.formula = `=IMPORTRANGE("https://docs.google.com/spreadsheets/d/${process.env.DAILY_SHEET_ID}", "'Detailed Daily Guide'!A2:JE237")`
+    await detailedA1.save()
 
     reporter.info('Updated dev Google Sheet successfully');
   } catch (error) {
