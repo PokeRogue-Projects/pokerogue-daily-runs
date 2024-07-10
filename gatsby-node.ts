e1a9a6e1e29670b22395bc0772ed5532d6deb2f0
coinmoles <andrewpark421@gmail.com>
2024-07-10 23:28:52 +0900

    add graphql custom schema

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -12,3 +12,70 @@ export const onCreateWebpackConfig = ({ actions }) => {
     },
   });
 };
+
+export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] = ({ actions }) => {
+  actions.createTypes(`
+    type DrpdJson implements Node @childOf(types: ["File", "Json"]) {
+      version: String!
+      title: String
+      authors: [String!]
+      date: String!
+      waves: [Wave!]!
+      starters: [Pokemon!]!
+    }
+
+    type Wave {
+      id: String!
+      reload: Boolean!
+      type: String!
+      double: Boolean!
+      actions: [String!]!
+      shop: String
+      biome: String!
+      trainer: Trainer
+      pokemon: [Pokemon!]
+    }
+
+    type Pokemon {
+      id: String!
+      name: String!
+      ability: String!
+      isHiddenAbility: Boolean!
+      passive: String!
+      nature: Nature!
+      gender: String!
+      rarity: String!
+      captured: Boolean!
+      level: Int!
+      items: [Item!]!
+      ivs: Ivs!
+    }
+
+    type Nature {
+      name: String!
+      increased: String!
+      decreased: String!
+    }
+
+    type Ivs {
+      hp: Int!
+      atk: Int!
+      def: Int!
+      spatk: Int!
+      spdef: Int!
+      spe: Int!
+    }
+    
+    type Item {
+      id: String!
+      name: String!
+      quantity: String!
+    }
+
+    type Trainer {
+      id: String!
+      name: String!
+      type: String!
+    }
+  `)
+}
