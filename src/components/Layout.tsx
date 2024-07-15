3c6974869aafe5b98397c3ec619c571bd717a8cf
coinmoles <andrewpark421@gmail.com>
2024-07-15 13:25:58 +0900

    re-implement date picker

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,14 +3,20 @@ import Header from "./Header";
 import Footer from "./Footer";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
-export default function Layout({ children }) {
-    return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <div className="flex flex-col min-h-screen">
-                <Header />
-                <div className="grow">{children}</div>
-                <Footer />
-            </div>
-        </ThemeProvider>
-    );
+export default function Layout({
+  children,
+  date,
+}: {
+  children: React.ReactNode;
+  date: string;
+}) {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex flex-col min-h-screen">
+        <Header date={date} />
+        <div className="grow">{children}</div>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
 }
