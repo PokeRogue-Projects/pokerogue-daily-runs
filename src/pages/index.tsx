cb4ff8249fc43a0f19217cb9daed4b42b77d647c
coinmoles <andrewpark421@gmail.com>
2024-07-18 20:04:51 +0900

    remove meta tag the meta tag was messing up the redirection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,21 +5,15 @@ import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
 
 const IndexPage: React.FC<PageProps<null>> = () => {
-    const currentDate = formatDate(
-        toZonedTime(new Date(Date.now()), "UTC"),
-        "yyyy-MM-dd"
-    );
+  const currentDate = formatDate(
+    toZonedTime(new Date(Date.now()), "UTC"),
+    "yyyy-MM-dd"
+  );
 
-    useEffect(() => {
-        navigate(`/runs/${currentDate}`);
-    }, []);
+  useEffect(() => {
+    navigate(`/runs/${currentDate}`);
+  }, []);
 
-    return (
-        <div>
-            <Helmet>
-                <meta httpEquiv="refresh" content={`0; /runs/${currentDate}`} />
-            </Helmet>
-        </div>
-    );
+  return <div></div>;
 };
 export default IndexPage;
