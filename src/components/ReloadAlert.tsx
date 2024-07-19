357f96bac055995ef7ad2e93277a81b5eb7b8c39
coinmoles <andrewpark421@gmail.com>
2024-07-19 09:53:40 +0900

    Fix reload instruction to say Ctrl+Shift+R instead of Shift+F5 Shift+F5 was a chrome specific feature

--- src/components/ReloadAlert.tsx
+++ src/components/ReloadAlert.tsx
@@ -9,6 +9,6 @@ export const ReloadAlert: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
   <Alert className={cn("bg-neutral-200 dark:bg-slate-800", className)}>
     <MessageCircleQuestion className="w-4 h-4" />
     <AlertTitle>Not Seeing a Run You Expected?</AlertTitle>
-    <AlertDescription>Try resetting the cache by pressing Shift + F5</AlertDescription>
+    <AlertDescription>Try resetting the cache by pressing Ctrl + Shift + R</AlertDescription>
   </Alert>
 );
